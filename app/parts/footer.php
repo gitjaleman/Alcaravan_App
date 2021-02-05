0e228f27783b4795022afbf3955675558d5192eb
PassGithub2020 <javiersettex@gmail.com>
2021-02-05 03:45:43 -0500

    estructura

---
+++ app/parts/footer.php
@@ -0,0 +1,16 @@
+  	</div>
+    <footer class="main-footer">
+			Copyright &copy; 2021-2025.
+      <div class="float-right d-none d-sm-inline-block">
+				Version 1.0
+      </div>
+    </footer>
+    <script src="../app/assets/js/jquery.min.js"></script>
+  	<script src="../app/assets/js/jquery-ui.min.js"></script>
+  	<script src="../app/assets/js/moment.min.js"></script>
+  	<script src="../app/assets/js/bootstrap.min.js"></script>
+  	<script src="../app/assets/js/adminlte.min.js"></script>
+ 	  <script src="../app/assets/js/demo.js"></script>
+  	<script src="../app/assets/js/dashboard.js"></script>
+	</body>
+</html>
--- app/parts/menu.php
+++ app/parts/menu.php
@@ -1,1 +1,136 @@
+<nav class="main-header navbar navbar-expand navbar-white navbar-light">
+	<ul class="navbar-nav">
+		<li class="nav-item">
+			<a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fa fa-bars"></i></a>
+		</li>
+	</ul>
+	<form class="form-inline ml-3">
+		<div class="input-group input-group-sm">
+			<input class="form-control form-control-navbar" type="search" placeholder="Search" aria-label="Search">
+			<div class="input-group-append">
+				<button class="btn btn-navbar" type="submit">
+					<i class="fa fa-search"></i>
+				</button>
+			</div>
+		</div>
+	</form>
+	<ul class="navbar-nav ml-auto">
+		<li class="nav-item">
+			<a class="nav-link" data-widget="fullscreen" href="#" role="button">
+				<i class="fa fa-expand-arrows-alt"></i>
+			</a>
+		</li>
+		<li class="nav-item">
+			<a class="nav-link" data-widget="control-sidebar" data-slide="true" href="#" role="button">
+				<i class="fa fa-th-large"></i>
+			</a>
+		</li>
+	</ul>
+</nav>
 
+<aside class="main-sidebar sidebar-dark-primary elevation-4">
+
+	<a href="index3.html" class="brand-link">
+		<img src="../app/assets/img/user2-160x160.jpg" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
+		<span class="brand-text font-weight-light">Alexander Pierce</span>
+	</a>
+
+	<div class="sidebar">
+		<br>
+		<br>
+		<nav class="mt-2">
+			<ul class="nav nav-pills nav-sidebar flex-column  text-sm" data-widget="treeview" role="menu" data-accordion="false">
+				<li class="nav-item">
+					<a href="" class="nav-link">
+						<i class="nav-icon fa fa-image"></i>
+						<p>
+							Gallery
+						</p>
+					</a>
+				</li>
+				<li class="nav-item">
+					<a href="" class="nav-link">
+						<i class="nav-icon fa fa-image"></i>
+						<p>
+							Gallery
+						</p>
+					</a>
+				</li>
+				<li class="nav-item">
+					<a href="" class="nav-link">
+						<i class="nav-icon fa fa-image"></i>
+						<p>
+							Gallery
+						</p>
+					</a>
+				</li>
+				<li class="nav-item">
+					<a href="" class="nav-link">
+						<i class="nav-icon fa fa-image"></i>
+						<p>
+							Gallery
+						</p>
+					</a>
+				</li>
+				<li class="nav-item">
+					<a href="" class="nav-link">
+						<i class="nav-icon fa fa-image"></i>
+						<p>
+							Gallery
+						</p>
+					</a>
+				</li>
+				<li class="nav-item">
+					<a href="" class="nav-link">
+						<i class="nav-icon fa fa-image"></i>
+						<p>
+							Gallery
+						</p>
+					</a>
+				</li>
+				<li class="nav-item">
+					<a href="" class="nav-link">
+						<i class="nav-icon fa fa-image"></i>
+						<p>
+							Gallery
+						</p>
+					</a>
+				</li>
+				<li class="nav-item">
+					<a href="" class="nav-link">
+						<i class="nav-icon fa fa-image"></i>
+						<p>
+							Gallery
+						</p>
+					</a>
+				</li>
+				<li class="nav-item">
+					<a href="" class="nav-link">
+						<i class="nav-icon fa fa-image"></i>
+						<p>
+							Gallery
+						</p>
+					</a>
+				</li>
+				<li class="nav-item">
+					<a href="" class="nav-link">
+						<i class="nav-icon fa fa-image"></i>
+						<p>
+							Gallery
+						</p>
+					</a>
+				</li>
+				<li class="nav-item">
+					<a href="" class="nav-link">
+						<i class="nav-icon fa fa-image"></i>
+						<p>
+							Gallery
+						</p>
+					</a>
+				</li>
+			</ul>
+		</nav>
+
+	</div>
+</aside>
+<div class="content-wrapper">
