585926891ceb9dae8fcee22c746bf37480e6eeeb
PassGithub2020 <javiersettex@gmail.com>
2021-02-24 17:05:28 -0500

    db

--- rest/lib/DB.php
+++ rest/lib/DB.php
@@ -1,7 +1,7 @@
 <?php
 	class conn extends mysqli{
 		function __construct(){
-			parent::__construct("localhost","root","","db_alcaravan");
+			parent::__construct("localhost","u717707398_alcaravan","Password.2021","u717707398_alcaravan");
 				if (mysqli_connect_error()) {
 					print("error de conexion");
 				}
