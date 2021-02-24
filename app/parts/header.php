04fb8f1e29e2d6e117cef663d1024c5c390b0dce
PassGithub2020 <javiersettex@gmail.com>
2021-02-24 17:36:46 -0500

    init index

--- app/parts/header.php
+++ app/parts/header.php
@@ -5,6 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <title>Alcaraván</title>
     <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport"/>
+    <link rel="icon" type="image/png" href="../app/assets/img/favicon.png">
     <link rel="stylesheet" href="../app/assets/css/bootstrap.min.css"/>
     <link rel="stylesheet" href="../app/assets/css/font-awesome.min.css"/>
     <link rel="stylesheet" href="../app/assets/css/dataTables.bootstrap.min.css" />
